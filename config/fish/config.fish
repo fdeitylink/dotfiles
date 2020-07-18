7148cedf984a5d8a06943743aee3d23e20c0fba4
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2020-07-18 12:12:32 -0400

    only set DISPLAY if running in WSL

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,4 @@
 fish_vi_key_bindings
-set -Ux DISPLAY (string join ":" (awk '/nameserver / {print $2; exit}' /etc/resolv.conf  2>/dev/null) "0.0")
+if grep -qi "microsoft" /proc/version
+    set -Ux DISPLAY (string join ":" (awk '/nameserver / {print $2; exit}' /etc/resolv.conf  2>/dev/null) "0.0")
+end
