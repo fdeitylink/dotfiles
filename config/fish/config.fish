b97444b012774620483d9ab948368bcd65dd4c63
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2020-07-18 16:24:18 -0400

    suppress output in WSL check

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,4 @@
 fish_vi_key_bindings
-if grep -qi "microsoft" /proc/version
+if grep -qsi "microsoft" /proc/version
     set -Ux DISPLAY (string join ":" (awk '/nameserver / {print $2; exit}' /etc/resolv.conf  2>/dev/null) "0.0")
 end
