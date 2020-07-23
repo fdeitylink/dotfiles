7cd6a554094a037626186db15a2fa0a52de062e6
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2020-07-22 22:39:34 -0400

    emacs: sync doom
    emacs: only install doom if not installed

---
+++ install-doom.fish
@@ -0,0 +1,10 @@
+function install-doom
+    echo "installing doom"
+    if not test -e ~/.emacs.d
+        git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+        ~/.emacs.d/bin/doom install
+        ~/.emacs.d/bin/doom sync
+    else
+        true
+    end
+end
