b69822248cb6964b3048de6bacd480d59c3f065a
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2021-04-09 19:02:38 -0400

    emacs: use -y flag in doom installer

--- install-doom.fish
+++ install-doom.fish
@@ -2,7 +2,7 @@ function install-doom
     echo "installing doom"
     if not test -e ~/.emacs.d
         git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-        yes | ~/.emacs.d/bin/doom install
+        ~/.emacs.d/bin/doom install -y
         ~/.emacs.d/bin/doom sync
     else
         true
