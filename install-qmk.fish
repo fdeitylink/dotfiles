2e3176711d178b8161fd01d09274d8734858a926
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2021-06-13 20:01:07 -0400

    qmk: clone submodules with qmk_firmware

--- install-qmk.fish
+++ install-qmk.fish
@@ -1,7 +1,7 @@
 function install-qmk
     echo "installing qmk_firmware"
     if not test -e ~/qmk_firmware
-        git clone --depth 1 git@github.com:qmk/qmk_firmware ~/qmk_firmware
+        git clone --depth 1 --recurse-submodules git@github.com:qmk/qmk_firmware ~/qmk_firmware
     else
         true
     end
