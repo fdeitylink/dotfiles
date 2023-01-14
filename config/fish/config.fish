57308777d64de5aa17cad0849a103c1bfc9b242a
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2023-01-14 16:37:54 -0500

    fish: remove WSL check

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,3 @@ fish_vi_key_bindings
 alias vim nvim
 alias vi nvim
 
-if grep -qsi microsoft /proc/version
-    set -Ux DISPLAY (string join ":" (awk '/nameserver / {print $2; exit}' /etc/resolv.conf  2>/dev/null) "0.0")
-end
