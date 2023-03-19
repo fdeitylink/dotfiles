f9b7023114b1890d2624b603b19916fe82501e68
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2023-03-19 17:53:11 -0400

    fish: move all config to config.fish
    alias tree to 'exa -T'

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,18 @@
-fish_vi_key_bindings
+set -gx EDITOR vim
+set -gx VISUAL emacs
+
+set -gx GPG_TTY (tty)
+
+set fish_user_paths ~/.local/bin ~/.emacs.d/bin
 
 alias vim nvim
 alias vi nvim
-
 alias ls exa
-
+alias tree 'exa -T'
 alias cat bat
+
+fish_vi_key_bindings
+fish_config theme choose 'Dracula Official'
+fish_config prompt choose 'scales'
+set -g fish_prompt_pwd_dir_length 0
+set -g fish_greeting
