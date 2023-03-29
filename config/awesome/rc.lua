09e506d44875b135dcdfdddbd989dbf5f97263a7
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2023-03-29 01:16:49 -0400

    awesome: update fennel init code for fennel 1.3.0

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -1,5 +1,8 @@
-local fennel = require("fennel")
-debug.traceback = fennel.traceback
-fennel.path = fennel.path .. ";.config/awesome/?.fnl"
-table.insert(package.loaders or package.searchers, fennel.searcher)
-require("config")
+function script_path()
+   return debug.getinfo(2, "S").source:sub(2):match("(.*/)")
+end
+
+package.path = package.path .. ";" .. script_path() .. "fennel-1.3.0/?.lua"
+fennel = require("fennel")
+fennel.install()
+fennel.dofile(script_path() .. "config.fnl")
