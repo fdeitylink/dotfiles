169daacfc281d4e110e21a7086b254baea307e5a
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2024-03-07 20:49:55 -0500

    awesome: remove config

--- config/awesome/rc.lua
+++
@@ -1,8 +0,0 @@
-function script_path()
-   return debug.getinfo(2, "S").source:sub(2):match("(.*/)")
-end
-
-package.path = package.path .. ";" .. script_path() .. "fennel-1.3.0/?.lua"
-fennel = require("fennel")
-fennel.install()
-fennel.dofile(script_path() .. "config.fnl")
