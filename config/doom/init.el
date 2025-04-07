b19b456f75c38a3faf26773ed015ecf1714557bc
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2025-04-07 00:45:10 -0400

    emacs: enable agda, ~~coq~~ rocq modules

--- config/doom/init.el
+++ config/doom/init.el
@@ -112,12 +112,12 @@
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
-       ;;agda              ; types of types of types of types...
+       (agda +local +tree-sitter) ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        ;;(cc +lsp)         ; C > C++ == 1
        (clojure +lsp +tree-sitter) ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
-       ;;coq               ; proofs-as-programs
+       coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
