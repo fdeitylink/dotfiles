4241389e18da6229e8e413a26809eeeed202b77d
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2025-09-25 13:58:17 -0400

    doom: update install script

--- install-doom.fish
+++ install-doom.fish
@@ -1,6 +1,6 @@
 function install-doom
     echo "installing doom"
-    if not test -e ~/.emacs.d
+    if not test -e ~/.config/emacs
         git clone --depth 1 git@github.com:doomemacs/doomemacs.git ~/.config/emacs
         doom -! install
     else
